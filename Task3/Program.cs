009406f42384d7bcf1aa72ceb37a8191085b2f7f
Iren <Iren010484@mail.ru>
2023-07-24 03:00:54 +0500

    Решение 3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу вычисления 
+функции Аккермана с помощью рекурсии.*/
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine()); 
+    return number;
+}
+
+int Akkerman(int a, int b) 
+{
+    if(a == 0)
+        {return b+1;} 
+        else if (a != 0 && b == 0)
+                 {return Akkerman(a-1,1);}
+                 else {return Akkerman(a-1,Akkerman(a,b - 1));}
+}
+
+int n = ReadInt("Введите Число N");
+int m = ReadInt("Введите Число M");
+
+Console.WriteLine($" Функция Аккермана равна {Akkerman(n,m)}");
