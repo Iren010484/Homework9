6ea6fff715f6673ce6a307be7bc02ad1f684ec98
Iren <Iren010484@mail.ru>
2023-07-24 02:14:07 +0500

    Решение 1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа от N до 1. 
+Выполнить с помощью рекурсии.*/
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine()); 
+    return number;
+}
+
+void NaturalNumbers(int a) 
+{
+if (a > 0)
+{
+    Console.Write($"  {a} ");
+    NaturalNumbers(a-1);
+}
+
+}
+
+int n = ReadInt("Введите Число N");
+
+NaturalNumbers(n);
+Console.WriteLine();
